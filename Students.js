c6bf0989716fa9431252a02f9d1e4c9cd4ae9016
Chaewon Yun <chaewonyun@mwc-124141.dhcp.missouri.edu>
2017-12-10 07:34:34 -0600

    exploration4 AppML

---
+++ Students.js
@@ -0,0 +1,19 @@
+{
+"system":[
+{"StudentName":"Jon Patman","Class":"CS4830","StudentNumber":"14357643"},
+{"StudentName":"Jiayu Xia","Class":"CS4770","StudentNumber":"15438763"},
+{"StudentName":"Lorenzo Mortelli","Class":"CS2830","StudentNumber":"13254797"},
+{"StudentName":"Jack Martin","Class":"CS4830","StudentNumber":"14326589"},
+{"StudentName":"William Morrison","Class":"CS3330","StudentNumber":"12309865"},
+{"StudentName":"Xiangyu Li","Class":"CS2050","StudentNumber":"16509254"},
+{"StudentName":"James Hurt","Class":"CS4290","StudentNumber":"13095876"},
+{"StudentName":"Morgan Knoch","Class":"CS4400","StudentNumber":"14398756"},
+{"StudentName":"Dan Huang","Class":"CS3580","StudentNumber":"13254376"},
+{"StudentName":"Morgan Knoch","Class":"CS4770","StudentNumber":"13209587"},
+{"StudentName":"Chaewon Yun","Class":"CS4830","StudentNumber":"14198069"},
+{"StudentName":"Cameron Warder","Class":"CS3330","StudentNumber":"14126578"},
+{"StudentName":"Haiyan Hu","Class":"CS4550","StudentNumber":"16325467"},
+{"StudentName":"Kevin Free","Class":"CS3330","StudentNumber":"12137649"},
+{"StudentName":"Kyle Hagan","Class":"CS4830","StudentNumber":"14120976"}
+]
+}
